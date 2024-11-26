e9da95dfc4c30cd9affdbe27cdc02259893d1082
Howard2503 <2429571337@qq.com>
2024-11-26 17:43:29 +0800

    删除注释代码

--- mahjong-game-v1-1/server/server.js
+++ mahjong-game-v1-1/server/server.js
@@ -17,30 +17,6 @@ app.use(express.static("public"));
 // 棋盘状态（一个简单的示例棋盘，可以根据实际需求扩展）
 let boardState = Array(100).fill(null); // 示例：9格棋盘
 
-// 监听客户端连接
-// io.on("connection", (socket) => {
-//   console.log(`Client connected: ${socket.id}`);
-
-//   // 发送初始棋盘状态给新连接的客户端
-//   socket.emit("initBoard", boardState);
-
-//   // 监听客户端发送的麻将牌移动操作
-//   socket.on("moveTile", (data) => {
-//     const { index, tile } = data;
-
-//     // 更新服务器端的棋盘状态
-//     boardState[index] = tile;
-
-//     // 广播操作给其他客户端（不包括当前客户端）
-//     socket.broadcast.emit("updateBoard", { index, tile });
-//   });
-
-//   // 监听客户端断开连接
-//   socket.on("disconnect", () => {
-//     console.log(`Client disconnected: ${socket.id}`);
-//   });
-// });
-
 io.on("connection", (socket) => {
   console.log(`Client connected: ${socket.id}`);
 
@@ -74,8 +50,6 @@ io.on("connection", (socket) => {
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-//这是个新line
 
 // 获取随机颜色
 function getRandomColor() {
